01071a1d5f91306848aa217400344a79399cdce8
Peter Jang <peter.x.jang@gmail.com>
2019-08-06 18:26:03 -0500

    Install axios and display posts from JSONPlaceholder API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      posts: []
+    };
+  },
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      console.log(response.data);
+      this.posts = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
